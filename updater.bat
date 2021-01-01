b16ff90cbce878142bfd269fd08a8ad55e71f49f
Shidiq Nur Hidayat <shidiq.hidayat@gmail.com>
2021-01-02 06:07:06 +0700

    add updater

--- updater.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-xcopy /y "D:\genose\software\genose_ai_rc\release\version" "D:\genose\software\share"
-xcopy /y "D:\genose\software\genose_ai_rc\compile win\GeNose Ai setup.exe" "D:\genose\software\share"
-
-robocopy "D:\genose\software\genose_ai_rc\compile win\requirements for Windows 10 (x64)" "D:\genose\software\share\requirements for Windows 10 (x64)" /mir
-
-pause
-exit
